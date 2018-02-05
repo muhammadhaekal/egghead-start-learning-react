1f920ad1f22a40f3467811f47eb27c48e6c2f5e8
Muhammad Haekal <93haekal@gmail.com>
2018-02-05 13:47:31 +0700

    02 - Write a HelloWorld React Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App = () => <h1>test</h1>
+//function component
+class App extends React.Component {
+  render(){
+    // return <h1>Hellowww</h1>
+    // similar to
+    return React.createElement('h1',null,'helloww')
+  }
+}
+
+// const App = () => <h1> Stateless component </h1>
 
 export default App
